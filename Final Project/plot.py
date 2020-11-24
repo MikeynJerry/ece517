7478b7db41a9b74f68abe8ec2c8eb4574c4d0153
Jerry <MikeynJerry@gmail.com>
2020-11-24 01:47:45 -0500

    Plotter Updates

--- Final Project/plot.py
+++ Final Project/plot.py
@@ -8,16 +8,31 @@ def load_stats(path):
         return json.load(f)
 
 
-def plot(experiments, data):
+def plot(experiments, datakeys, as_subplots):
     paths = [f"experiments/{experiment}/stats.json" for experiment in experiments]
     stats = [load_stats(path) for path in paths]
 
-    for datakey in data:
-        for experiment, name in zip(stats, experiments):
-            plt.plot(range(len(experiment[datakey])), experiment[datakey], label=name)
-        plt.legend()
-        plt.title(datakey)
+    if as_subplots:
+        fig, axes = plt.subplots(ncols=len(datakeys), figsize=(4 * len(datakeys), 5))
+        if len(datakeys) == 1:
+            axes = [axes]
+        for i, datakey in enumerate(datakeys):
+            for experiment, name in zip(stats, experiments):
+                axes[i].plot(
+                    range(len(experiment[datakey])), experiment[datakey], label=name
+                )
+            axes[i].legend()
+            axes[i].set_title(datakey)
         plt.show()
+    else:
+        for datakey in datakeys:
+            for experiment, name in zip(stats, experiments):
+                plt.plot(
+                    range(len(experiment[datakey])), experiment[datakey], label=name
+                )
+            plt.legend()
+            plt.title(datakey)
+            plt.show()
 
 
 if __name__ == "__main__":
@@ -25,8 +40,9 @@ if __name__ == "__main__":
 
     parser.add_argument("-e", "--experiments", nargs="+")
     parser.add_argument(
-        "--data", nargs="+", default=["avg_loss", "loss_vs_iteration", "eps"]
+        "--datakeys", nargs="+", default=["avg_loss", "loss_vs_iteration", "eps"]
     )
+    parser.add_argument("--as-subplots", action="store_true")
 
     args = parser.parse_args()
 
